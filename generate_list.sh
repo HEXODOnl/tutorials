ed26646c8b17012f679051faa03eab5a533ee4af
CircleCI <circleci@0fi.net>
2015-04-27 15:45:15 +0200

    Generated list

--- generate_list.sh
+++ generate_list.sh
@@ -11,15 +11,13 @@ done;
 for path in en/tutorials/*/*-*.md
 do
 	IFS='/' read -a explode <<< "$path"
-	category=${explode[2]}
-	category="$(tr '[:lower:]' '[:upper:]' <<< ${category:0:1})${category:1}"
 	title=$(cat $path | sed -n 1p)
 	title="${title/'# '/}"
 	author=$(cat $path | sed -n 2p)
 	author="${author/'By: '/}"
 	date=`stat -c %y $path | cut -d ' ' -f1`
 	url="${explode[1]}/${explode[2]}/${explode[3]}"
-	echo "| $category | [$title]($url) | $author | $date | " >> ${explode[0]}/${explode[1]}/${explode[2]}/index.md
+	echo "[$title]($url) | $author | $date | " >> ${explode[0]}/${explode[1]}/${explode[2]}/index.md
 	echo ${explode[0]}/${explode[1]}/${explode[2]}
 done;
 
