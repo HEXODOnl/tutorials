8740b0350741ca5540cb3a6fa2ab9839e4f87576
CircleCI <circleci@0fi.net>
2015-04-27 15:41:26 +0200

    Generated list

--- en/tutorials/vpses/index.md
+++ en/tutorials/vpses/index.md
@@ -1,3 +1,6 @@
 | Title | Author | Date |
 | ----- | ------ | ---- |
 | [](tutorials/vpses/) |  | 2015-04-27 | 
+
+| Title | Author | Date |
+| ----- | ------ | ---- |
--- generate_list.sh
+++ generate_list.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
+cat tutorials_template.md > en/list.md
 for path in en/tutorials/*/
 do
-	cat category_template.md > $path/index.md
-	echo '' >> en/list.md
+	echo '' >> $path/index.md
 	echo "| Title | Author | Date |" >> $path/index.md
 	echo "| ----- | ------ | ---- |" >> $path/index.md
+done;
+
+for path in en/tutorials/*/*.md
+do
 	IFS='/' read -a explode <<< "$path"
+	category=${explode[2]}
+	category="$(tr '[:lower:]' '[:upper:]' <<< ${category:0:1})${category:1}"
 	title=$(cat $path | sed -n 1p)
 	title="${title/'# '/}"
 	author=$(cat $path | sed -n 2p)
 	author="${author/'By: '/}"
 	date=`stat -c %y $path | cut -d ' ' -f1`
 	url="${explode[1]}/${explode[2]}/${explode[3]}"
-	echo "| [$title]($url) | $author | $date | " >> $path/index.md
+	echo "| $category | [$title]($url) | $author | $date | " >> en/list.md
 done;
 
 cat tutorials_template.md > en/list.md
