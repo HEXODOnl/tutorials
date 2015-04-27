645840fda2139dc86100fba82b0ac92b598bc3c7
CircleCI <circleci@0fi.net>
2015-04-27 15:48:40 +0200

    Generated list

--- generate_list.sh
+++ generate_list.sh
@@ -25,25 +25,6 @@ do
 	echo ${explode[0]}/${explode[1]}/${explode[2]}
 done;
 
-cat tutorials_template.md > en/list.md
-echo '' >> en/list.md
-echo "| Category | Title | Author | Date |" >> en/list.md
-echo "| -------- |------ | ------ | ---- |" >> en/list.md
-
-for path in en/tutorials/*/*.md
-do
-	IFS='/' read -a explode <<< "$path"
-	category=${explode[2]}
-	category="$(tr '[:lower:]' '[:upper:]' <<< ${category:0:1})${category:1}"
-	title=$(cat $path | sed -n 1p)
-	title="${title/'# '/}"
-	author=$(cat $path | sed -n 2p)
-	author="${author/'By: '/}"
-	date=`stat -c %y $path | cut -d ' ' -f1`
-	url="${explode[1]}/${explode[2]}/${explode[3]}"
-	echo "| $category | [$title]($url) | $author | $date | " >> en/list.md
-done;
-
 git config --global user.email "circleci@0fi.net"
 git config --global user.name "CircleCI"
 
