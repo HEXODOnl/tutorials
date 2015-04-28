47fd3991603a2e7bee8b1b50d207cff349ffde6f
Cas de Reuver <me@cas.yt>
2015-04-28 16:40:24 +0200

    Date -> Difficulty

--- generate_list.sh
+++ generate_list.sh
@@ -8,8 +8,8 @@ do
 	category="$(tr '[:lower:]' '[:upper:]' <<< ${category:0:1})${category:1}"
 	echo '' > $path/index.md
 	echo "# Categories -> $category" >> $path/index.md
-	echo "| Title | Author | Date |" >> $path/index.md
-	echo "| ----- | ------ | ---- |" >> $path/index.md
+	echo "| Title | Author | Difficulty |" >> $path/index.md
+	echo "| ----- | ------ | ---------- |" >> $path/index.md
 	echo "- [$category](${explode[1]}/${explode[2]}/index.md)" >> en/list.md
 done;
 
@@ -20,9 +20,10 @@ do
 	title="${title/'# '/}"
 	author=$(cat $path | sed -n 2p)
 	author="${author/'By: '/}"
-	date=`stat -c %y $path | cut -d ' ' -f1`
+	difficulty=$(cat $path | sed -n 2p)
+	difficulty="${difficulty/'Difficulty: '/}"
 	url="${explode[3]}"
-	echo "| [$title]($url) | $author | $date | " >> ${explode[0]}/${explode[1]}/${explode[2]}/index.md
+	echo "| [$title]($url) | $author | $difficulty | " >> ${explode[0]}/${explode[1]}/${explode[2]}/index.md
 	echo ${explode[0]}/${explode[1]}/${explode[2]}
 done;
 
